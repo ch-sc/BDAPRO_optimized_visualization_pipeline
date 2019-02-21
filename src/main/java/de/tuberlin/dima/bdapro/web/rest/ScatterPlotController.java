a78ad3acb5ec1ca52a051758e2441884f1943d44
Christoph Schulze <chr_schulze@freenet.de>
2019-02-21 15:46:08 +0100

    Clean up code

--- src/main/java/de/tuberlin/dima/bdapro/web/rest/ScatterPlotController.java
+++ src/main/java/de/tuberlin/dima/bdapro/web/rest/ScatterPlotController.java
@@ -4,14 +4,19 @@ package de.tuberlin.dima.bdapro.web.rest;
 import java.io.IOException;
 import java.io.OutputStream;
 import javax.servlet.http.HttpServletResponse;
+import javax.websocket.server.PathParam;
 
 import de.tuberlin.dima.bdapro.data.taxi.StreamDataProcessor;
 import de.tuberlin.dima.bdapro.error.ErrorType;
 import de.tuberlin.dima.bdapro.error.ErrorTypeException;
+import de.tuberlin.dima.bdapro.model.ExecutionType;
 import de.tuberlin.dima.bdapro.service.DataService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,35 +36,57 @@ public class ScatterPlotController {
 	}
 	
 	
-	@GetMapping(value = "/scatter")
-	public int[][] scatterPlot(@RequestParam(value = "x", required = false) Integer x,
+	@ModelAttribute("bounds")
+	public DimensionalityBounds parameterPreProcessing(@RequestParam(value = "x", required = false) Integer x,
 			@RequestParam(value = "y", required = false) Integer y) throws ErrorTypeException {
-		
-		if (x == null && y == null) {
-			return dataService.scatterPlot();
-		} else if (x == null) {
-			x = y;
-		} else if (y == null) {
-			y = x;
-		} else if (x <= 0 || y <= 0) {
+		if ((x != null && x <= 0) || (y != null && y <= 0)) {
 			throw new ErrorTypeException(ErrorType.PARAMETER_ERROR, "Only positive values are allowed.");
 		}
 		
-		return dataService.scatterPlot(x, y);
+		return new DimensionalityBounds(x, y);
+	}
+	
+	
+	@GetMapping(value = "/scatter")
+	public int[][] scatterPlot(@ModelAttribute("bounds") DimensionalityBounds bounds) throws ErrorTypeException {
+		if (bounds.isUnbound()) {
+			return dataService.scatterPlot();
+		}
+		return dataService.scatterPlot(bounds.x, bounds.y);
 	}
 	
 	
 	@GetMapping(value = "/scatter/stream")
-	public void scatterPlot(@RequestParam(value = "x", required = false, defaultValue = "1000") Integer xDim,
-			@RequestParam(value = "y", required = false, defaultValue = "1000") Integer yDim,
+	public void scatterPlot(@ModelAttribute("bounds") DimensionalityBounds bounds,
 			HttpServletResponse response) {
 		
 		try (OutputStream out = response.getOutputStream()) {
-			dataService.scatterPlot(xDim, yDim, out);
+			dataService.scatterPlot(bounds.x, bounds.y, out);
 		} catch (IOException e) {
 			throw new RuntimeException(ExceptionUtils.getMessage(e), e);
 		}
 	}
 	
 	
+	static class DimensionalityBounds {
+		
+		private Integer x;
+		private Integer y;
+		
+		
+		DimensionalityBounds(Integer x, Integer y) {
+			if (y == null) {
+				y = x;
+			} else if (x == null) {
+				x = y;
+			}
+			this.x = x;
+			this.y = y;
+		}
+		
+		
+		boolean isUnbound() {
+			return x == null && y == null;
+		}
+	}
 }
